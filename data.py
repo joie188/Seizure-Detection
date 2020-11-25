55b5cc0b138588b3f305e4e74910cc250c14799d
Joie Le <joiele@mit.edu>
2020-11-24 17:29:47 -0800

    Get simple stats

--- data.py
+++ data.py
@@ -1,12 +1,31 @@
-import json
 import pandas as pd
 
 
 def read_csv(csv_filename):
-    # read data file and assign column names
+    # read data file
     data = pd.read_csv(csv_filename)
-    # remove unnecessary columns
-    data = data.drop(labels=[],
-                     axis='columns')
 
-    return data
+    # remove unnecessary columns
+    # data.drop(data.columns[0], axis=1, inplace=True)
+
+    # assign binary label 
+    data['label'] = (data.y == 1).astype(int)
+
+    # FEATURES
+    features = pd.DataFrame()
+    features['min'] = data.min(axis=1)
+    features['max'] = data.max(axis=1)
+    features['mean'] = data.mean(axis=1) 
+    features['median'] = data.median(axis=1) 
+    #features['mode'] = data.mode(axis=1) 
+    features['std'] = data.std(axis=1)
+    # line length 
+    # entropy 
+    # energy
+    # peak frequency
+
+    return features
+
+
+data = read_csv("./data.csv")
+print(data)
